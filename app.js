2459297521e6c1ba516f91a375ae37e60ec506a5
brohxa <64731231+brohxa@users.noreply.github.com>
2020-05-14 20:26:02 +0530

    Added typewriter aanimation

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // ANIMATION FOR DROP DOWN HEADING
 
-gsap.from(".main-heading", {
-  y: -50,
-  opacity: 0,
-  duration: 2,
-  ease: Power1.easeOut,
-});
 gsap.from("nav", {
   x: -30,
   opacity: 0,
