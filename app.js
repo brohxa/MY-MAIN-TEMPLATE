339c1b71c68e610bbae9ba88ee67ba4013692407
brohxa <64731231+brohxa@users.noreply.github.com>
2020-05-16 03:45:34 +0530

    BARELY FINISHED BLOG HAVE TO ADD ANIMATION

--- app.js
+++ app.js
@@ -36,9 +36,11 @@ let aboutBtn = document.querySelector(".about-click");
 
 aboutBtn.addEventListener("click", function aboutIsActive() {
   let portfolio = document.querySelector(".portfolio");
+  let blog = document.querySelector(".blog");
   let about = document.querySelector(".about");
   about.classList.toggle("about-active");
   portfolio.classList.remove("portfolio-active");
+  blog.classList.remove("blog-active");
 });
 
 // PORTFOLIO COME OUT
@@ -47,7 +49,22 @@ let portfolioBtn = document.querySelector(".portfolio-click");
 
 portfolioBtn.addEventListener("click", function portfolioIsActive() {
   let about = document.querySelector(".about");
+  let blog = document.querySelector(".blog");
   let portfolio = document.querySelector(".portfolio");
   portfolio.classList.toggle("portfolio-active");
   about.classList.remove("about-active");
+  blog.classList.remove("blog-active");
+});
+
+// BLOG COME OUT
+
+let blogBtn = document.querySelector(".blog-click");
+
+blogBtn.addEventListener("click", function blogIsActive() {
+  let about = document.querySelector(".about");
+  let blog = document.querySelector(".blog");
+  let portfolio = document.querySelector(".portfolio");
+  blog.classList.toggle("blog-active");
+  about.classList.remove("about-active");
+  portfolio.classList.remove("portfolio-active");
 });
